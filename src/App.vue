6416b510d4ae064870aad3ac7144be4e87c654ab
Fabrice Geib <fabricegeib@gmail.com>
2019-12-25 23:28:33 +0100

    scss and mixin

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,32 @@
 
 <style lang="scss">
 @import "@/assets/global-styles/colors.scss";
+@import "@/assets/global-styles/typography.scss";
+
+body {
+  background-color: $dark-blue;
+}
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
+  color: $white;
 }
+
+// h1 {
+//   @include heading-3;
+// }
 
 #nav {
   padding: 30px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    // color: #2c3e50;
+    color: $white;
 
     &.router-link-exact-active {
       color: #42b983;
